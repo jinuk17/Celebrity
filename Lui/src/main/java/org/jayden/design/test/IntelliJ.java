9818b9c6d4b8dc205104f572f11ccc43a66b7f23
jaydenuk <jayden.uk@daumkakao.com>
2015-09-01 17:38:29 +0900

    IntelliJ test

---
+++ Lui/src/main/java/org/jayden/design/test/IntelliJ.java
@@ -0,0 +1,12 @@
+package org.jayden.design.test;
+
+/**
+ * Created by jaydenuk on 2015. 9. 1..
+ */
+public class IntelliJ {
+
+    public void test(){
+        System.out.println("So difficult.");
+    }
+}
+
